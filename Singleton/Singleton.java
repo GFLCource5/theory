19bdfbef7e0dd60ebdc2cfff8b9302dcbbc1bea9
Oleksii <mail.alexbondarenko@gmail.com>
2023-08-03 16:16:05 +0300

    add Singleton pattern

---
+++ Singleton/Singleton.java
@@ -0,0 +1,20 @@
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {
+        if (instance != null) {
+            throw new IllegalStateException("Not permitted to construct with reflection");
+        }
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
