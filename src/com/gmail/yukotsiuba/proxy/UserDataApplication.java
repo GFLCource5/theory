80540184474a0eda86458fbc4eefb119355deb8e
ykotsiuba <jurok3x@gmail.com>
2023-08-14 15:17:00 +0300

    added proxy

---
+++ src/com/gmail/yukotsiuba/proxy/UserDataApplication.java
@@ -0,0 +1,21 @@
+package com.gmail.yukotsiuba.proxy;
+
+
+public class UserDataApplication {
+    public static void main(String[] args) {
+        UserDataServiceImpl userDataService = new UserDataServiceImpl();
+        UserData userData1 = userDataService.getUserData(1);
+        UserData userData2 = userDataService.getUserData(2);
+
+        UserDataServiceCacheImpl userDataServiceCache = new UserDataServiceCacheImpl();
+        UserData cacheUser1 = userDataServiceCache.getUserData(3);
+        UserData cacheUser2 = userDataServiceCache.getUserData(4);
+        UserData cacheUser3 = userDataServiceCache.getUserData(4);
+
+        System.out.println(userData1);
+        System.out.println(userData2);
+        System.out.println(cacheUser1);
+        System.out.println(cacheUser2);
+        System.out.println(cacheUser3);
+    }
+}
---
+++ src/com/gmail/yukotsiuba/proxy/UserDataService.java
@@ -0,0 +1,5 @@
+package com.gmail.yukotsiuba.proxy;
+
+public interface UserDataService {
+    UserData getUserData(Integer id);
+}
