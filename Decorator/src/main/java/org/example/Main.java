a6170494c24fe6582ef29c8027bd11571a25a99d
Sokol3783 <sokolplus3783@gmail.com>
2023-10-02 22:40:14 +0300

    simple example of using facade

--- Decorator/src/main/java/org/example/Main.java
+++ Decorator/src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> list = new ArrayList<>();
+        list.addAll(List.of(1,2,3,4,5,6,7,8,9,10));
+        OddList oddList = new OddListImpl(list);
+
+        System.out.println("Usual list first element:");
+        System.out.println(list.get(0));
+        System.out.println("Odd list first element:");
+        System.out.println(oddList.get(0));
+
+        System.out.println("Usual list second element:");
+        System.out.println(list.get(1));
+        System.out.println("Odd list second element:");
+        System.out.println(oddList.get(1));
+
+        System.out.println("Usual list size:");
+        System.out.println(list.size());
+        System.out.println("Odd list size:");
+        System.out.println(oddList.size());
     }
 }
--- Decorator/src/main/java/org/example/OddListImpl.java
+++ Decorator/src/main/java/org/example/OddListImpl.java
@@ -2,8 +2,9 @@ package org.example;
 
 import java.util.AbstractList;
 import java.util.List;
+import java.util.stream.Stream;
 
-public class OddListImpl<Integer> extends AbstractList implements OddList {
+public class OddListImpl<T extends Integer> extends AbstractList implements OddList {
 
     private final List<Integer> list;
 
@@ -13,14 +14,17 @@ public class OddListImpl<Integer> extends AbstractList implements OddList {
 
     @Override
     public Object get(int index) {
-
-        list.stream().filter(s -> )
-
-
+        List<Integer> filtered = filterList();
+        return filtered.get(index);
     }
 
     @Override
     public int size() {
-        return 0;
+        return filterList().size();
     }
+
+    private List<Integer> filterList(){
+        return list.stream().filter(s -> s % 2 != 0).toList();
+    }
+
 }
