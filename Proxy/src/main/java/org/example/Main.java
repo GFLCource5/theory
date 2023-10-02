320f9c9ea4c9b204968ecc3ebc4424ebfae27352
Sokol3783 <sokolplus3783@gmail.com>
2023-10-02 22:17:49 +0300

    init commit

---
+++ Proxy/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Screamer screamer = new Signaling();
+        System.out.println(screamer);
+
+    }
+}
---
+++ Proxy/src/main/java/org/example/Signaling.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Signaling implements Screamer{
+    @Override
+    public final String cream() {
+        return "wiy-wiy";
+    }
+
+}
