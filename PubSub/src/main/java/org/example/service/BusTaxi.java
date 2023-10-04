3b667df14876a4289e1f6c23191d70cdfe511e89
Sokol3783 <sokolplus3783@gmail.com>
2023-10-04 21:12:41 +0300

    write implementation

--- PubSub/src/main/java/org/example/service/BusTaxi.java
+++ PubSub/src/main/java/org/example/service/BusTaxi.java
@@ -3,10 +3,14 @@ package org.example.service;
 import org.example.model.Passenger;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BusTaxi implements Taxi{
     @Override
     public void ride(List<Passenger> passengers) {
-
+        if (!passengers.isEmpty()) {
+            String collect = passengers.stream().map(Passenger::getName).collect(Collectors.joining());
+            System.out.println("Bus taxi transport: " + collect);
+        }
     }
 }
