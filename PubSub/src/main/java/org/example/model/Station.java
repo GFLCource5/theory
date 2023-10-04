896103aba589a633ee86b5e333efa9d547c67b30
Sokol3783 <sokolplus3783@gmail.com>
2023-10-04 19:57:40 +0300

    I make Station singleton

--- PubSub/src/main/java/org/example/model/Station.java
+++ PubSub/src/main/java/org/example/model/Station.java
@@ -5,4 +5,13 @@ import java.util.List;
 
 public class Station<T extends Passenger> extends ArrayList implements List {
 
+    private Station<T> instance;
+
+    public synchronized Station<T> getInstance(){
+        if (instance == null){
+            instance = new Station<>();
+        }
+        return instance;
+    }
+
 }
