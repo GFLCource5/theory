a16d79cb641cce2e8807122792bd985445423d85
Sokol3783 <sokolplus3783@gmail.com>
2023-10-04 19:47:55 +0300

    add contract of pub/sub pattern

--- PubSub/src/main/java/org/example/Subscriber.java
+++ PubSub/src/main/java/org/example/Subscriber.java
@@ -1,4 +1,9 @@
 package org.example;
 
+import org.example.model.Passenger;
+
 public interface Subscriber {
+
+    void getPassenger(Passenger... passengers);
+
 }
---
+++ PubSub/src/main/java/org/example/model/Station.java
@@ -0,0 +1,8 @@
+package org.example.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Station<T extends Passenger> extends ArrayList implements List {
+
+}
