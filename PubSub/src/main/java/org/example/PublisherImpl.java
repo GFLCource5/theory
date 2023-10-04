6693f8db9743c31edf534751698e920dbfc2b193
Sokol3783 <sokolplus3783@gmail.com>
2023-10-04 21:13:01 +0300

    refactor station

--- PubSub/src/main/java/org/example/PublisherImpl.java
+++ PubSub/src/main/java/org/example/PublisherImpl.java
@@ -4,17 +4,18 @@ import org.example.model.Passenger;
 import org.example.model.Station;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class PublisherImpl implements Publisher{
 
-    private final Station<Passenger> passengers;
+    private final List<Passenger> passengers;
 
-    public PublisherImpl(Station<Passenger> passengers) {
+    public PublisherImpl(List<Passenger> passengers) {
         this.passengers = passengers;
     }
 
     @Override
     public void registerPassengers(Passenger... passengers) {
-        this.passengers.getInstance().addAll(Arrays.asList(passengers));
+        this.passengers.addAll(Arrays.asList(passengers));
     }
 }
--- PubSub/src/main/java/org/example/SubscriberImpl.java
+++ PubSub/src/main/java/org/example/SubscriberImpl.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import org.example.model.Passenger;
-import org.example.model.Station;
 import org.example.service.BusTaxi;
 import org.example.service.SingleTaxi;
 import org.example.service.Taxi;
@@ -12,18 +11,18 @@ import java.util.stream.Collectors;
 
 public class SubscriberImpl implements Subscriber{
 
-    private final Station<Passenger> passengers;
+    private final List<Passenger> passengers;
 
-    public SubscriberImpl(Station<Passenger> passengers) {
+    public SubscriberImpl(List<Passenger> passengers) {
         this.passengers = passengers;
     }
 
     @Override
     public void transportPassengers() {
 
-        if (!passengers.getInstance().isEmpty()) {
+        if (!passengers.isEmpty()) {
             transportAllGockingsWithWind();
-            if (passengers.getInstance().size() == 1){
+            if (passengers.size() == 1){
                 transportPassengersSingleTaxi();
             } else {
                 transportPassengersBusTaxi();
@@ -34,18 +33,18 @@ public class SubscriberImpl implements Subscriber{
     }
 
     private void transportPassengersBusTaxi() {
-        getBusTaxi().ride(passengers.getInstance());
-        passengers.getInstance().clear();
+        getBusTaxi().ride(passengers);
+        passengers.clear();
     }
 
     private void transportPassengersSingleTaxi() {
-        getSingleTaxi().ride(passengers.getInstance());
-        passengers.getInstance().clear();
+        getSingleTaxi().ride(passengers);
+        passengers.clear();
     }
 
     private void transportAllGockingsWithWind() {
-        List<Passenger> gockings = passengers.getInstance().stream().filter(s -> s.getName().contains("Gockings")).collect(Collectors.toList());
-        passengers.getInstance().removeAll(gockings);
+        List<Passenger> gockings = passengers.stream().filter(s -> s.getName().contains("Gockings")).collect(Collectors.toList());
+        passengers.removeAll(gockings);
         getTaxiWithWind().ride(gockings);
     }
 
