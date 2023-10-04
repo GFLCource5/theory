210ef354eb943ffacee9c08708eb77e20d872207
Sokol3783 <sokolplus3783@gmail.com>
2023-10-04 19:31:30 +0300

    add model

---
+++ PubSub/src/main/java/org/example/model/Passenger.java
@@ -0,0 +1,16 @@
+package org.example.model;
+
+public class Passenger {
+
+    private final String name;
+
+
+    public Passenger(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
