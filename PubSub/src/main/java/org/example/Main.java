7119d0aca71647d042a162c2e0857d173d5a4cf5
Sokol3783 <sokolplus3783@gmail.com>
2023-10-04 21:13:33 +0300

    add sout for clear example this implementation

--- PubSub/src/main/java/org/example/Main.java
+++ PubSub/src/main/java/org/example/Main.java
@@ -1,7 +1,30 @@
 package org.example;
 
+import org.example.model.Passenger;
+import org.example.model.Station;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Bus station start work!");
+        Publisher publisher = new PublisherImpl(Station.getInstance());
+        Subscriber subscriber = new SubscriberImpl(Station.getInstance());
+
+        publisher.registerPassengers(new Passenger("Team Cleski"));
+        subscriber.transportPassengers();
+        System.out.println("\n\n");
+
+        publisher.registerPassengers(new Passenger("Team Cleski Junior"), new Passenger("Sten Gockings"));
+        subscriber.transportPassengers();
+        System.out.println("\n\n");
+
+
+        publisher.registerPassengers(new Passenger("Team Cleski"), new Passenger("Team Cleski Junior"), new Passenger("Sten Gockings"));
+        subscriber.transportPassengers();
+        System.out.println("\n\n");
+
+        publisher.registerPassengers(new Passenger("Martha Gockings"), new Passenger("Team Cleski"), new Passenger("Team Cleski Junior"), new Passenger("Sten Gockings"));
+        subscriber.transportPassengers();
+        System.out.println("\n\n");
+
     }
 }
