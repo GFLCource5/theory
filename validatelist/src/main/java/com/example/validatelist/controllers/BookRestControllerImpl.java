fd65a974845a52ad82072c8414f68a70aec44ccf
Sokol3783 <sokolplus3783@gmail.com>
2023-10-07 02:26:41 +0300

    validate works for single and in list

--- validatelist/src/main/java/com/example/validatelist/controllers/BookRestControllerImpl.java
+++ validatelist/src/main/java/com/example/validatelist/controllers/BookRestControllerImpl.java
@@ -2,14 +2,18 @@ package com.example.validatelist.controllers;
 
 import com.example.validatelist.entity.Book;
 import com.example.validatelist.service.BookService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-public class BookRestControllerImpl implements BookRestController {
+@Validated
+public class BookRestControllerImpl {
 
     private  final  BookService service;
 
@@ -17,15 +21,13 @@ public class BookRestControllerImpl implements BookRestController {
         this.service = service;
     }
 
-    @Override
     @PostMapping("/book/")
-    public ResponseEntity<Book> addBook(Book book) {
+    public ResponseEntity<Book> addBook(@RequestBody @Valid Book book) {
         return ResponseEntity.ok(service.createBook(book));
     }
 
-    @Override
-    @PostMapping("/books/many/")
-    public ResponseEntity<List<Book>> addBooks(List books) {
+    @PostMapping("/book/many/")
+    public ResponseEntity<List<Book>> addBooks(@RequestBody List<@Valid Book> books) {
         return ResponseEntity.ok(service.createBooks(books));
     }
 }
