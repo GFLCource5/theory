5b04db025b5d9a7225956bbed26630d8494141ea
Sokol3783 <sokolplus3783@gmail.com>
2023-10-14 15:47:51 +0300

    make number generator computable

--- async/src/main/java/com/example/async/AsyncService.java
+++ async/src/main/java/com/example/async/AsyncService.java
@@ -5,19 +5,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class AsyncService {
 
     private static final Logger logger = LoggerFactory.getLogger(AsyncService.class);
 
-    public int generateNumber() throws InterruptedException {
+    public CompletableFuture<Integer> generateNumber() throws InterruptedException {
         logger.info("Start generate" + Thread.currentThread().getName());
         int number = LocalDateTime.now().getSecond();
         int randomNumber = number - getRandomInt(number/3, number/2);
         Thread.sleep(randomNumber);
         logger.info("Finish generate" + Thread.currentThread().getName());
-        return randomNumber;
+        return CompletableFuture.completedFuture(randomNumber);
     }
 
     private int getRandomInt(int min, int max) {
