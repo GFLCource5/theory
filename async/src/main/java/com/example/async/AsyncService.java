a1637be894e2a3a068ff41f678271aec61eac3c1
Sokol3783 <sokolplus3783@gmail.com>
2023-10-14 15:44:08 +0300

    add async service

---
+++ async/src/main/java/com/example/async/AsyncService.java
@@ -0,0 +1,27 @@
+package com.example.async;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class AsyncService {
+
+    private static final Logger logger = LoggerFactory.getLogger(AsyncService.class);
+
+    public int generateNumber() throws InterruptedException {
+        logger.info("Start generate" + Thread.currentThread().getName());
+        int number = LocalDateTime.now().getSecond();
+        int randomNumber = number - getRandomInt(number/3, number/2))
+        Thread.sleep(randomNumber);
+        logger.info("Finish generate" + Thread.currentThread().getName());
+        return randomNumber;
+    }
+
+    private int getRandomInt(int min, int max) {
+        return (int) (min + Math.random() * max);
+    }
+
+}
