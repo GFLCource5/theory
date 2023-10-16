5f475e6882634ebba7b3788273f4d57b18740a99
Sokol3783 <sokolplus3783@gmail.com>
2023-10-16 16:32:12 +0300

    future

--- ThreadPool/src/main/java/org/example/App.java
+++ ThreadPool/src/main/java/org/example/App.java
@@ -1,13 +1,28 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) throws InterruptedException {
+        List<String> list = new ArrayList<>();
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        executorService.submit(() -> list.add("First submit " + Thread.currentThread().getName()));
+        executorService.submit(() -> list.add("Second submit " + Thread.currentThread().getName()));
+        executorService.submit(() -> list.add("Third submit " + Thread.currentThread().getName()));
+
+        executorService.awaitTermination(3, TimeUnit.SECONDS);
+
+        list.forEach(System.out::println);
+
+        executorService.shutdown();
+
     }
+
 }
